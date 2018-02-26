7c1782c27f2e9f5be2be0a11553b67907c8528e6
ConorobDIT <c14733295@mydit.ie>
2018-02-26 17:35:27 +0000

    Add files via upload

--- Lab 2/JWT/index.js
+++ Lab 2/JWT/index.js
@@ -4,23 +4,15 @@ const sequelize = require("sequelize");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const secret = "$329239dksdkdjkw"
+const secret = "$329239dksdkdjkw" //Random secret key.
 
 const app = express();
 const response = (status, message, data=undefined) => {
-  return { status, message, data };
+  return { status, message, data }; //Response for error messages
 }
 app.use(bodyParser.json());
 
-const conntectionInfo = {
-    host: "127.0.0.1",
-    port: 5432,
-    database: "jwtDB",
-    user: 'postgres',
-    password: 'pass',
-};
-
-var Sequelize = new sequelize('jwtDB', 'postgres', 'pass', {
+var Sequelize = new sequelize('jwtDB', 'postgres', 'pass', { //Signing in with sequelize to the database
     host: '127.0.0.1',
     dialect: 'postgres',
     pool: {
@@ -42,53 +34,37 @@ Sequelize
 
     // This endpoint must be publicly-accessible
     app.post('/register', (req, res) => {
-      const email = req.body.email;
-      const password = req.body.password;
-      const username = req.body.username;
       const insert = "INSERT INTO users (email, password, username) VALUES \
         (:email, crypt(:password, gen_salt('bf', 8)), :username);";
-        Sequelize.query(insert, { replacements: { email, password, username }, type: sequelize.QueryTypes.INSERT }).then(() => {
-        res.json({})
+        Sequelize.query(insert, { replacements: { email: [req.body.email], password: [req.body.password], username: [req.body.username] }, type: sequelize.QueryTypes.INSERT }).then(() => {
+        res.json({message: "Successfully registered"}) 
       })
     });
 
-  /*  app.post('/login', (req, res) => {
-      const email = req.body.email;
-      const password = req.body.password;
-      const user = {
-      email1: email,
-      password1: password
-  }
+  app.post('/login', (req, res) => {
       const query = 'SELECT * FROM users WHERE email = lower(:email) AND \
         password = crypt(:password, password);';
-        req.app.get('db').query(query, { replacements: { email, password } })
+        Sequelize.query(query, { replacements: { email: [req.body.email], password: [req.body.password] }, type: sequelize.QueryTypes.SELECT })
         .then((result) => {
           if (result[0].length == 0) {
             res
             .status(401)
             .json(response('Failed', 'Incorrect password or email'))
           } else {
+            const user = {email: [req.body.email], password: [req.body.password]};
+
             jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
               res.json({
+                message: "Success, here is your token",
                 token
                 });
-            const token = auth.generateToken(result[0][0])
-            res.send(response('Sucess', 'Successful Authentication', {
-              'token': token
-            }))
-          }
-        })
-        .catch(() => {
-          res
-            .status(401)
-            .json(response('Failed', 'Incorrect password or email'))
+            })}
+          })
         });
-    });
 
     // This endpoint is publicly-accessible (by choice)
-    app.get("/products/:id", (req, res) => {
-        req.app.get('db')
-            .query("select * from products where id = $1", [req.params.id])
+    app.post("/products/:id", (req, res) => {
+        Sequelize.query("select * from products where id = :id", {replacements: {id: [req.body.id]}, type: sequelize.QueryTypes.SELECT})
             .then(items => {
                 res.json(items);
             })
@@ -97,22 +73,34 @@ Sequelize
 
     // This endpoint must be protected with authentication
     app.post("/products", authenticate, (req, res) => {
-        // Not actually implemented
-        res.status(200).send("Successfully authenticated");
-    });*/
+          jwt.verify(req.token, 'secretkey', (err, authData) => {
+          if(err) {
+            res.sendStatus(403);
+          } else {
+            Sequelize.query(("select * from products"), {type: sequelize.QueryTypes.SELECT})
+                .then(items => {
+                    res.json(items);
+            });
+    }
+  });
+    });
 
 
 // Authentication middleware for protected endpoints
 function authenticate(req, res, next) {
-    /*
-     * The steps for completing this function are as follows:
-     *
-     *  1. Extract the authorization header from req.headers
-     *  2. The needs to be of the form:
-     *         BEARER JWT-TOKEN
-     *  3. Parse the authorization header to extract the value of JWT-TOKEN
-     *  4. Call the jwt.verify() function passing this and the public key
-     *  5. If they match then call next(), otherwise respond with a 401
-     */
-    // YOUR CODE GOES HERE
-}
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }}
